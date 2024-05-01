de8461b71baa2464c44a7be997a796953eb771d5
Joao <joaogabriel443@gmail.com>
2024-05-01 16:49:12 -0300

    Testes feitos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿﻿using System.Text;
+using DesafioPOO.Models;
 
+Console.OutputEncoding = Encoding.UTF8;
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("16999999999", "Iphone 14 pro max", "12345678494443", 256000);
+Nokia nokia = new Nokia("16998888888", "Nokia Tijolão", "05891238137789", 8000);
+
+Console.WriteLine("-------------------------------------------------------------------------");
+
+Console.WriteLine("Testes Iphone: ");
+Console.WriteLine("Numero: " + iphone.Numero);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("-------------------------------------------------------------------------");
+
+Console.WriteLine("Testes Nokia: ");
+Console.WriteLine("Numero: " + nokia.Numero);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
